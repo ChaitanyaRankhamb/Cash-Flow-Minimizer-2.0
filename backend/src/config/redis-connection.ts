ff8437615daaf734cc197aaf89194544a50c7358
ChaitanyaRankhamb54 <chaitanyarankhamb007@gmail.com>
2026-02-26 02:00:14 +0530

    change client to redisClient name

--- backend/src/config/redis-connection.ts
+++ backend/src/config/redis-connection.ts
@@ -1,18 +1,18 @@
 import { createClient } from "redis";
 
-const client = createClient({
+const redisClient = createClient({
   url: "redis://localhost:6379",
   // url: "redis://redis-stack:6379"  for docker based redis connection
 });
 
 
-client.on("error", (err) =>
+redisClient.on("error", (err) =>
   console.log("Redis Client Error", err),
 );
 
 export const redisConnection = async () => {
-  await client.connect();
+  await redisClient.connect();
   console.log("Redis connected successfully!");
 };
 
-export default client;
+export default redisClient;
