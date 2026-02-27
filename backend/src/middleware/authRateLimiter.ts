c28cde8dc1a17f89b7c8fbcadaaed2735d7fe061
ChaitanyaRankhamb54 <chaitanyarankhamb007@gmail.com>
2026-02-27 11:26:21 +0530

    Implemented Redis rate limiter.

--- backend/src/middleware/authRateLimiter.ts
+++ backend/src/middleware/authRateLimiter.ts
@@ -1,12 +1,35 @@
-import rateLimit from 'express-rate-limit';
+import { Request, Response, NextFunction } from "express";
+import { RateLimiterRedis, RateLimiterRes } from "rate-limiter-flexible";
+import redisClient from "../config/redis-connection";
 
-export const authRateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // limit each IP to 5 requests
-  message: {
-    message: "Too many login attempts. Try again after 15 minutes."
-  },
-  standardHeaders: true,
-  legacyHeaders: false,
-  keyGenerator: (req) => req.ip + (req.body.email || ""),
+const redisRateLimiter = new RateLimiterRedis({
+  storeClient: redisClient,  // connect with redis client
+  useRedisPackage: true, // required for node-redis v4+
+  keyPrefix: "auth_v2",
+  points: 5, // 5 max request
+  duration: 15 * 60, // 15 minutes in seconds
 });
+
+export const authRateLimiter = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const key = req.ip || "unknown";
+
+  try {
+    await redisRateLimiter.consume(key);
+    next();
+  } catch (error) {
+    if (error instanceof RateLimiterRes) {
+      return res.status(429).json({
+        message: "Too many requests. Please try again later.",
+      });
+    }
+
+    console.error("Auth rate limiter internal error:", error);
+    return res.status(500).json({
+      message: "Internal server error. Please try again later.",
+    });
+  }
+};
